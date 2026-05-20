d03e5b0d82b906ce9333c34d0b38005c84332107
Alan Richardson <monkeyboysmash23@gmail.com>
2026-05-19 17:38:43 -0700

    Add jump animation and play it on jump; update editor path

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,27 +13,24 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle erratic jump: jump height is slightly unpredictable
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		var unpredictable_jump = randf_range(0.7, 1.3)
 		velocity.y = JUMP_VELOCITY * unpredictable_jump
+		animated_sprite.play("jump")
 
 	# Get input direction
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
-		# Weird acceleration: sluggishly drift into max speed
 		var target_speed = direction * MAX_SPEED
 		velocity.x = move_toward(velocity.x, target_speed, ACCELERATION * delta)
 		
 		animated_sprite.play("walk")
 		animated_sprite.flip_h = direction < 0
 	else:
-		# Loose, slippery deceleration: feels like you have no balance
 		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
 		
 		# Only play idle if we are somewhat stable
 		animated_sprite.play("idle")
 
 	move_and_slide()
-
