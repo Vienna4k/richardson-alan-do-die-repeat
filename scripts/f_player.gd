632afa697a79ba9bb4dc21ba0710f5740d1119b1
Vienna <104324786+Vienna4k@users.noreply.github.com>
2026-06-08 12:45:49 -0700

    fixed problems with player

--- scripts/f_player.gd
+++ scripts/f_player.gd
@@ -1,17 +1,20 @@
 extends RigidBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-# Grab references to the new leg bodies. 
-# (Make sure these names exactly match what you named them in the editor!)
 @onready var left_leg: RigidBody2D = $LeftLeg
 @onready var right_leg: RigidBody2D = $RightLeg
+@onready var floor_raycast: RayCast2D = $FloorRayCast  # We will use this to detect the floor!
 
-# Feel free to adjust these values in the editor or here to make it feel right
-const ASSIST_FORCE: float = 600.0
-const LEG_TORQUE: float = 12000.0
-const STAND_TORQUE: float = 20000.0
-const JUMP_IMPULSE: float = -350.0
+const ASSIST_FORCE: float = 1000.0
+const LEG_TORQUE: float = 30000.0
+const STAND_TORQUE: float = 30000.0
+const JUMP_IMPULSE: float = -1050.0
+const STRIDE_SPEED: float = 24.0
+
+const STAND_ANGLE_LEFT: float = deg_to_rad(10.0)  # Splay left leg slightly outward
+const STAND_ANGLE_RIGHT: float = deg_to_rad(-10.0)  # Splay right leg slightly outward
+
+var run_time: float = 0.0
 
 func _ready() -> void:
 	animated_sprite.animation_finished.connect(_on_animation_finished)
@@ -30,13 +33,10 @@ func _on_animation_finished() -> void:
 		_start_blink_timer()
 
 func _physics_process(delta: float) -> void:
-	# RigidBody2D applies gravity automatically, so we skip the gravity addition here!
 
-	# Handle jump (Using a basic velocity check since RigidBody2D doesn't have `is_on_floor()`)
-	# Later, if the jumping feels inconsistent, we can add a RayCast2D pointing down to act as floor detection.
 	if Input.is_action_just_pressed("jump"):
-		# If the character is not falling/rising super fast, let them jump
-		if abs(linear_velocity.y) < 20.0:
+		# Only jump if the RayCast is actually hitting the floor
+		if floor_raycast.is_colliding():
 			apply_central_impulse(Vector2(0.0, JUMP_IMPULSE))
 
 	var direction: float = Input.get_axis("left", "right")
@@ -45,17 +45,32 @@ func _physics_process(delta: float) -> void:
 		# 1. Apply the "assist" to the main body
 		apply_central_force(Vector2(direction * ASSIST_FORCE, 0.0))
 		
-		# 2. Spin the legs! Applying torque will rotate the rigidbodies around their center mass / joints
-		# Depending on the friction and shape, spinning them moves the player QWOP-style
-		left_leg.apply_torque(direction * LEG_TORQUE)
-		right_leg.apply_torque(direction * LEG_TORQUE)
-	else:
-		# Stand the legs back up when not moving!
-		# angle_difference finds the shortest path to 0 rotation (straight down)
-		var left_diff: float = angle_difference(left_leg.rotation, 0.0)
-		var right_diff: float = angle_difference(right_leg.rotation, 0.0)
+		# 2. Drive the legs as synchronized motors! This creates a guaranteed, perfectly timed stride.
+		run_time += direction * delta * STRIDE_SPEED
 		
-		# Apply a strong corrective torque to push them towards 0
+		# The left leg targets the current motor time, the right leg is permanently offset by PI (180 degrees)
+		var left_target: float = run_time
+		var right_target: float = run_time + PI
+		
+		var left_diff: float = angle_difference(left_leg.rotation, left_target)
+		var right_diff: float = angle_difference(right_leg.rotation, right_target)
+		
+		left_leg.apply_torque(left_diff * LEG_TORQUE)
+		right_leg.apply_torque(right_diff * LEG_TORQUE)
+		
+		# Dampening keeps them tightly synced to the stride timer instead of violently overshooting
+		left_leg.apply_torque(-left_leg.angular_velocity * 1000.0)
+		right_leg.apply_torque(-right_leg.angular_velocity * 1000.0)
+	else:
+		# When jumping from a standstill or just turning around, we don't want the legs to snap to 0 immediately
+		# Let's reset the run target to whatever the leg average is so that taking off again is smooth
+		run_time = (left_leg.rotation + right_leg.rotation) / 2.0
+		
+		# Stand the legs back up into a stable 'A' frame pose
+		var left_diff: float = angle_difference(left_leg.rotation, STAND_ANGLE_LEFT)
+		var right_diff: float = angle_difference(right_leg.rotation, STAND_ANGLE_RIGHT)
+		
+		# Apply a strong corrective torque to push them towards their standing angles
 		left_leg.apply_torque(left_diff * STAND_TORQUE)
 		right_leg.apply_torque(right_diff * STAND_TORQUE)
 		
