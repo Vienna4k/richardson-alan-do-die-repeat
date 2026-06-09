fdd72f41217cebd40d5481c576b65012a85cbec5
Vienna <104324786+Vienna4k@users.noreply.github.com>
2026-06-09 01:26:39 -0700

    Update audio setup, add BigCoin & export preset

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -22,8 +22,13 @@ func _on_finish_line_body_entered(body: Node2D) -> void:
 		print("Good Job!")
 		win_label.show()
 		win_music.play()
+		#grab the background music global and pause the music
+		var bgm_player := get_node("/root/Bgm/TimeForAdventure") as AudioStreamPlayer
+		bgm_player.stream_paused = true
 		Engine.time_scale = 0
 
 		await win_music.finished
 		Engine.time_scale = 1
+		#resume background music after finish before scene load
+		bgm_player.stream_paused = false
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
